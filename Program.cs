caa3529c0c01c6327a01014bd82534d7137d1b0a
mariolip <117418756+mariolip@users.noreply.github.com>
2023-01-29 00:14:42 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+//throw new Exception();
+int z;
+z = 1;
+Console.WriteLine("z is " + z);
+Console.WriteLine("Hello, Sky!");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
